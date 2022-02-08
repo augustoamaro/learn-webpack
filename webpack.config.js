5ef71d4d7a03148aaa43a1aa12b6f66c96b67e07
Augusto <augustoamaro@icloud.com>
2022-02-08 17:06:31 -0300

    Added sass in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
+      }
+    ]
   }
 };
